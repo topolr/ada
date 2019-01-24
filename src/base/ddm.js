41a71588ac88f88c955a5b9f1be49d08a6767248
wangjinliang <wangjinliang@huobi.com>
2019-01-24 17:22:25 +0800

    pass

--- src/base/ddm.js
+++ src/base/ddm.js
@@ -1053,7 +1053,7 @@ let Effecter = {
 		}
 	},
 	effect(context, dom, actions) {
-		context.logger.log(`> DIFF : Badd:${actions.badd.length} Add:${actions.add.length} Replace:${actions.replace.length} Remove:${actions.remove.length} Edit:${actions.edit.length} RemoveAll:${actions.removeAll.length} Bremove:${actions.bremove.length} Sort:${actions.sort.length}`);
+		context.logger.log(`> DIFF[${actions.badd.length + actions.add.length + actions.replace.length + actions.remove.length + actions.edit.length + actions.removeAll.length + actions.bremove.length + actions.sort.length}] : Badd:${actions.badd.length} Add:${actions.add.length} Replace:${actions.replace.length} Remove:${actions.remove.length} Edit:${actions.edit.length} RemoveAll:${actions.removeAll.length} Bremove:${actions.bremove.length} Sort:${actions.sort.length}`);
 		let adds = {};
 		actions.badd.forEach(action => {
 			let t = dom;
