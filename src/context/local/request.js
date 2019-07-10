5fa114e6b2c6e7dc3a2c91cecd396bfb55c28b4c
hou80houzhu <topolr@vip.qq.com>
2019-07-10 23:37:24 +0800

    pass

--- src/context/local/request.js
+++ src/context/local/request.js
@@ -1,4 +1,4 @@
-let {randomid} = require("./../../util/helper");
+let { randomid } = require("./../../util/helper");
 
 class RequestMananger {
     constructor(context, worker) {
@@ -6,15 +6,15 @@ class RequestMananger {
         this._worker = worker;
         this._requestTransformer = [];
         this._responseTransformer = [];
-        this.addResponseTransformer((response) => {
-            let r = response.data;
-            if (r[0] === '[' || r[0] === '{') {
+        this._responseDataFilter = [];
+        this.addResponseDataFilter(data => {
+            if (data[0] === '[' || data[0] === '{') {
                 try {
-                    r = JSON.parse(r);
+                    data = JSON.parse(data);
                 } catch (e) {
                 }
             }
-            return r;
+            return data;
         });
     }
 
@@ -63,6 +63,23 @@ class RequestMananger {
         }
     }
 
+    addResponseDataFilter(fn) {
+        if (this._responseDataFilter.indexOf(fn) === -1) {
+            this._responseDataFilter.push(fn);
+        }
+    }
+
+    removeResponseDataFilter(fn) {
+        let index = this._responseDataFilter.indexOf(fn);
+        if (index !== -1) {
+            this._responseDataFilter.splice(index, 1);
+        }
+    }
+
+    cleanResponseDataFilter() {
+        this._responseDataFilter = [];
+    }
+
     cleanRequestTransformer() {
         this._requestTransformer = [];
     }
@@ -72,7 +89,7 @@ class RequestMananger {
     }
 
     fetch(option) {
-        return this.invoke(option).promise;
+        return this.invoke(option);
     }
 
     origin(option) {
@@ -80,43 +97,46 @@ class RequestMananger {
     }
 
     invoke(option) {
-        let ops = RequestMananger.getOption(option);
-        this._requestTransformer.forEach(a => a(ops));
-        let {promise, cancel} = this._worker(this._context, ops);
-        let ps = promise.then(response => {
-            return this._responseTransformer.reduce((a, b) => {
-                return b(response, a);
-            }, null);
+        return Promise.resolve().then(() => {
+            return this._requestTransformer.reduce((a, b) => {
+                return a.then((c) => Promise.resolve().then(() => b(c)));
+            }, Promise.resolve(RequestMananger.getOption(option)));
+        }).then(ops => {
+            let { promise } = this._worker(this._context, ops);
+            return promise.then(response => {
+                return this._responseTransformer.reduce((a, b) => {
+                    return a.then((c) => Promise.resolve().then(() => b(c)));
+                }, Promise.resolve(response));
+            }).then(response => this._responseDataFilter.reduce((a, b) => b(a), response.data));
         });
-        return {promise: ps, cancel};
     }
 
     get(url, data = {}) {
-        return this.fetch(Object.assign({url, data}, {method: "get"}));
+        return this.fetch(Object.assign({ url, data }, { method: "get" }));
     }
 
     post(url, data = {}) {
-        return this.fetch(Object.assign({url, data}, {method: "post"}));
+        return this.fetch(Object.assign({ url, data }, { method: "post" }));
     }
 
     put(url, data = {}) {
-        return this.fetch(Object.assign({url, data}, {method: "put"}));
+        return this.fetch(Object.assign({ url, data }, { method: "put" }));
     }
 
     patch(url, data = {}) {
-        return this.fetch(Object.assign({url, data}, {method: "patch"}));
+        return this.fetch(Object.assign({ url, data }, { method: "patch" }));
     }
 
     head(url, data = {}) {
-        return this.fetch(Object.assign({url, data}, {method: "head"}));
+        return this.fetch(Object.assign({ url, data }, { method: "head" }));
     }
 
     options(url, data = {}) {
-        return this.fetch(Object.assign({url, data}, {method: "options"}));
+        return this.fetch(Object.assign({ url, data }, { method: "options" }));
     }
 
     upload(url, data = {}, onuploadprogress = null) {
-        return this.origin(Object.assign({url, data, contentType: false, onuploadprogress}, {method: "post"}));
+        return this.origin(Object.assign({ url, data, contentType: false, onuploadprogress }, { method: "post" }));
     }
 }
 
